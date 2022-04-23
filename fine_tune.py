10c086da924a79b6ade09524dfd134a05ba1b55b
Jaya Mundra <jm8834@nyu.edu>
2022-04-23 00:47:45 -0400

    fine tune model

---
+++ fine_tune.py
@@ -0,0 +1,71 @@
+# Feel free to modifiy this file. 
+# It will only be used to verify the settings are correct 
+# modified from https://pytorch.org/docs/stable/data.html
+
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+import transforms as T
+import utils
+from engine import train_one_epoch, evaluate
+
+
+from dataset import UnlabeledDataset, LabeledDataset
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+def get_model(num_classes):
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, pretrained_backbone=False)
+
+    #model = torchvision.models.resnet50(pretrained = False)
+    checkpoint = torch.load('path')
+    model.load_state_dict(checkpoint)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
+
+def main():
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    num_classes = 100
+    train_dataset = LabeledDataset(root='/labeled', split="training", transforms=get_transform(train=True))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2, collate_fn=utils.collate_fn)
+
+    valid_dataset = LabeledDataset(root='/labeled', split="validation", transforms=get_transform(train=False))
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=2, shuffle=False, num_workers=2, collate_fn=utils.collate_fn)
+
+    model = get_model(num_classes)
+    model.to(device)
+
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+    num_epochs = 1
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, valid_loader, device=device)
+
+    print("That's it!")
+
+if __name__ == "__main__":
+    main()
